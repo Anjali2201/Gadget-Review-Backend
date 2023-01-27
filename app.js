fd5710881ca19bc9b97ddc62ba1635be0aaf361d
Anjali2201 <anjalikushwaha031@gmail.com>
2023-01-27 21:15:36 +0530

    connected to mongodb

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from "express";
-
+import mongoose from "mongoose";
+// import blogRouter from "./routes/blog-routes";
+// import router from "./routes/user-routes";
+import cors from "cors";
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
-});
-
+app.use(cors());
+app.use(express.json());
+// app.use("/api/user", router);
+// app.use("/api/blog", blogRouter);
+mongoose
+  .connect(
+    "mongodb+srv://Anjali:amdra2201@cluster.6rnblhw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => app.listen(3000))
+  .then(() =>
+    console.log("Connected TO Database and Listening TO Localhost 5000")
+  )
+  .catch((err) => console.log(err));
