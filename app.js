a5ea12f21c42c5dead9fa80fb0ca668ac40e2ec5
Anjali2201 <anjalikushwaha031@gmail.com>
2023-01-27 21:37:16 +0530

    added user model, controller and routes

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
 // import blogRouter from "./routes/blog-routes";
 // import router from "./routes/user-routes";
 import cors from "cors";
 const app = express();
+
+dotenv.config();
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
 app.use(express.json());
 // app.use("/api/user", router);
 // app.use("/api/blog", blogRouter);
 mongoose
-  .connect(
-    "mongodb+srv://Anjali:amdra2201@cluster.6rnblhw.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => app.listen(3000))
   .then(() =>
-    console.log("Connected TO Database and Listening TO Localhost 5000")
+    console.log("Connected TO Database and Listening TO Localhost 3000")
   )
   .catch((err) => console.log(err));
