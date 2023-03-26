ef562c904567c8babac6cfb84999ba7291ed7d5d
Anjali2201 <anjalikushwaha031@gmail.com>
2023-03-27 04:26:41 +0530

    minor changes to app.js

--- app.js
+++ app.js
@@ -28,9 +28,13 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+app.listen(port, () => {
+  console.log(`Listening on port ${port}..`);
+});
+
 //  view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
@@ -43,10 +47,6 @@ app.use("/api/user/signup", signup);
 app.use("/api/user/login", login);
 
 // Starter and Error Listen Statesments
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}..`);
-});
 
 //catch 404 and forward to error handler
 app.use(function (req, res, next) {
